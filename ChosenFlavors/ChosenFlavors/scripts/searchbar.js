75995a9e5291728c40200bd07f219e82ab76edc9
Bao Tran <77811454+kbreaker1314@users.noreply.github.com>
2021-04-10 13:23:08 -0700

    added picture and drag function

--- ChosenFlavors/ChosenFlavors/scripts/searchbar.js
+++ ChosenFlavors/ChosenFlavors/scripts/searchbar.js
@@ -2,6 +2,6 @@
     var node = document.createElement("Li");
     var text = document.getElementById("searchtext").value;
     var textnode = document.createTextNode(text);
-    node.appendChild(textnode);
+    node.appendChild(textnode); 
     document.getElementById("list").appendChild(node);
 }
