e63d87e3453acefe88ce4927dda45069dec5db59
Bao Tran <77811454+kbreaker1314@users.noreply.github.com>
2021-04-10 12:50:12 -0700

    added function to add, to add ingridient on a list

--- ChosenFlavors/ChosenFlavors/scripts/searchbar.js
+++ ChosenFlavors/ChosenFlavors/scripts/searchbar.js
@@ -1,1 +1,7 @@
-﻿
+﻿document.getElementById("add").onclick = function () {
+    var node = document.createElement("Li");
+    var text = document.getElementById("searchtext").value;
+    var textnode = document.createTextNode(text);
+    node.appendChild(textnode);
+    document.getElementById("list").appendChild(node);
+}
